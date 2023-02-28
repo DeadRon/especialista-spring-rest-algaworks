11a83cccdb571d31c545c0aff5ebce7c3884d088
Ronaldo Melo <ronaldorpm1994@gmail.com>
2023-02-27 23:57:55 -0300

    8.13. Tratando excecoes globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -18,9 +18,6 @@ public class CadastroCidadeService {
 	public static final String MSG_CIDADE_EM_USO = "Cidade de código %d não pode ser removida, pois está em uso";
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
 
 	@Autowired
 	private CadastroEstadoService cadastroEstadoService;
